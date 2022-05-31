bff3a5c6c2b92110f8721fa166dfd8184d83afbb
Lakshya garg <42773324+lakshyagarg96@users.noreply.github.com>
2022-05-30 19:33:18 -0500

    Time: 21 ms (85.22%), Space: 13.5 MB (39.08%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -20,7 +20,7 @@ class Solution(object):
         while n!= 1:
             n = square(n)
             counter = counter + 1
-            if counter > 10:
+            if counter > 8:
                 flag = 0
                 break
             # print (n)
