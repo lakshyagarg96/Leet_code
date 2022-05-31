a7cf4a652fe2b47ba4b17d078d6fce5ebce3a4f0
Lakshya garg <42773324+lakshyagarg96@users.noreply.github.com>
2022-05-30 19:32:20 -0500

    Time: 368 ms (5.09%), Space: 13.2 MB (97.59%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,35 @@
+
+def square(n):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    counter = 0
+    for i in str(n):
+        counter = counter + int(i)**2
+    return counter 
+    
+class Solution(object):
+
+    def isHappy(self,n):
+        counter = 0
+        flag = 1
+        if n == 1:
+            return True
+        
+        while n!= 1:
+            n = square(n)
+            counter = counter + 1
+            if counter > 1000:
+                flag = 0
+                break
+            # print (n)
+            
+        if flag == 0:
+            return False
+        else:
+            return True
+        
+        
+    
+        
